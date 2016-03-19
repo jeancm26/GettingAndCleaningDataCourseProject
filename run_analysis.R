25d2b83040bd71d5fbe30c87909d6c878414478d
Jean Machado <jeanmachado26@gmail.com>
2016-03-19 17:49:34 -0500

    Second Commit Ajustes sobre el examen

--- run_analysis.R
+++ run_analysis.R
@@ -1,33 +1,41 @@
-rm(list = ls())
-Features <- read.table(file = "./UCI HAR Dataset/features.txt")
-#Leer registros Test
-SubjectTest <- read.table(file = "./UCI HAR Dataset/test/subject_test.txt")
-X_Test <- read.table(file = "./UCI HAR Dataset/test/X_test.txt")
-Y_Test <- read.table(file = "./UCI HAR Dataset/test/Y_test.txt")
-#Combinar todos los objetos a Test
-Test <- X_Test
-names(x = Test) <- paste(Features$V2, "_Test", sep = "")
-Test<-subset(x = Test, select = grepl(pattern = "mean", x = names(Test)) | grepl(pattern = "std", x = names(Test)))
-Test$Y_Test <- Y_Test$V1
-Test$Subject_Test <- SubjectTest$V1
+library(plyr)
 
-#Leer registros Train
-SubjectTrain <- read.table(file = "./UCI HAR Dataset/train/subject_train.txt", nrows = 2947)
-X_Train <- read.table(file = "./UCI HAR Dataset/train/X_train.txt", nrows = 2947)
-Y_Train <- read.table(file = "./UCI HAR Dataset/train/Y_train.txt", nrows = 2947)
-#Combinar todos los objetos a Test
-Train <- X_Train
-names(x = Train) <- paste(Features$V2, "_Train", sep = "")
-Train<-subset(x = Train, select = grepl(pattern = "mean", x = names(Train)) | grepl(pattern = "std", x = names(Train)))
-Train$Y_Train <- Y_Train$V1
-Train$Subject_Train <- SubjectTrain$V1
+# 1.- Merges the training and the test sets to create one data set.
+DataTrain <- read.table("./train/X_train.txt")
+LabelTrain <- read.table("./train/y_train.txt")
+SubjectTrain <- read.table("./train/subject_train.txt")
+DataTest <- read.table("./test/X_test.txt")
+LabelTest <- read.table("./test/y_test.txt")
+SubjectTest <- read.table("./test/subject_test.txt")
+#All Data
+Data <- rbind(DataTest, DataTrain)
+Label <- rbind(LabelTest, LabelTrain)
+Subject <- rbind(SubjectTest, SubjectTrain)
 
-Data <- cbind(Test, Train)
+# 2.- Extracts only the measurements on the mean and standard deviation for each measurement. 
+Features <- read.table("./features.txt")
+Data <- Data[,grep(pattern =  "mean\\(\\)|std\\(\\)",x =  Features$V2)]
 
-Subjects<-sapply(unique(Data$Subject_Train), function(x) Data[Data$Subject_Train == x,])
-Subjects<-sapply(X = Subjects[,], FUN = mean, simplify = TRUE)
-write.table(x = Subjects, file = "Train.txt", row.names = FALSE)
+# 3.- Uses descriptive activity names to name the activities in the data set
+Activity <- read.table("./activity_labels.txt")
+Activity[, 2] <- tolower(gsub("_", "", Activity[, 2]))
+substr(Activity[2, 2], 8, 8) <- toupper(substr(Activity[2, 2], 8, 8))
+substr(Activity[3, 2], 8, 8) <- toupper(substr(Activity[3, 2], 8, 8))
+LabelActivity <- Activity[Label[, 1], 2]
+Label[, 1] <- LabelActivity
+names(Label) <- "Activity"
+names(Data) <- gsub("\\(\\)", "", Features[grep(pattern =  "mean\\(\\)|std\\(\\)",x =  Features$V2), 2])
+names(Data) <- gsub("mean", "Mean", names(Data))
+names(Data) <- gsub("std", "Std", names(Data))
+names(Data) <- gsub("-", "", names(Data))
 
-#Borrar Registros
-rm(list = c("X_Test", "Y_Test", "Features", "SubjectTest", "X_Train", "Y_Train", "SubjectTrain"))
+# 4.- Appropriately labels the data set with descriptive activity names. 
+names(Subject) <- "Subject"
+Data <- cbind(Subject, Label, Data)
 
+# 5.- Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
+DataAverage <- ddply(Data, .(Subject, Activity), function(x) colMeans(x[, 3:68]))
+write.table(x = DataAverage, file = "Data.txt", row.names = FALSE)
+
+#Erase Test and Train Data
+rm(list = c("DataTest","DataTrain","LabelTest","LabelTrain", "SubjectTest", "SubjectTrain", "Activity", "LabelActivity"))
